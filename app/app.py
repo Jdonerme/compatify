c398ca161d7b336b0d051d4aa758637bf531e619
hatooku <jhlee@caltech.edu>
2016-04-16 00:27:17 -0700

    token works for one person

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, json
 import spotipy
+import json
 import random, math
 from spotipy import oauth2
 
@@ -8,10 +9,6 @@ app = Flask(__name__)
 
 
 # Methods
-def htmlForLoginButton():
-    auth_url = sp_oauth.get_authorize_url()
-    htmlLoginButton = "<a href='" + auth_url + "'>Login to Spotify</a>"
-    return htmlLoginButton
 
 def generateRandomState(length): 
     text = '';
@@ -32,11 +29,11 @@ PORT_NUMBER = 8888
 SPOTIPY_CLIENT_ID = '883896384d0c4d158bab154c01de29db'
 SPOTIPY_CLIENT_SECRET = '37443ee0c0404c44b755f3ed97c48493'
 SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
-SCOPE = 'user-library-read'
+SCOPE = 'user-library-read playlist-modify-public'
 CACHE = '.spotipyoauthcache'
 STATE = generateRandomState(16)
 
-sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI,state=STATE,scope=SCOPE,cache_path=CACHE )
+sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI,state=STATE,scope=SCOPE,cache_path=CACHE,show_dialog=True)
 
 
 
@@ -48,37 +45,18 @@ sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,SPOTIPY
 @app.route('/')
 @app.route('/index')
 def index():       
-    """
-    access_token = ""
-
-    token_info = sp_oauth.get_cached_token()
-
-    if token_info:
-        print "Found cached token!"
-        access_token = token_info['access_token']
-    else:
-        url = request.url
-        code = sp_oauth.parse_response_code(url)
-        if code:
-            print "Found Spotify auth code in Request URL! Trying to get valid access token..."
-            token_info = sp_oauth.get_access_token(code)
-            access_token = token_info['access_token']
-
-    if access_token:
-        print "Access token available! Trying to get user information..."
-        sp = spotipy.Spotify(access_token)
-        results = sp.current_user()
-        return results
-
-    else:
-        """
-    #return htmlForLoginButton()
     auth_url = sp_oauth.get_authorize_url()
     return render_template("index.html", auth_url=auth_url)
 
 @app.route('/callback')
 def callback():
-    string = "<p>state: " + request.args.get('state') + "</p><p>code: " + request.args.get('code') + "</p>"
+    code = request.args.get('code')
+    state = request.args.get('state')
+    token = sp_oauth.get_access_token(code)
+    sp = spotipy.Spotify(auth=token)
+    tracks1 = sp.current_user_saved_tracks(limit=20, offset=0) 
+
+    string = "<p>state: " + state + "</p><p>code: " + code + "</p><p>token: " + json.dumps(token) + "</p>"
     return string
 
 
