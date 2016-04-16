5d574816db38c7cdc847a6eb1eb6ca18d1080571
hatooku <jhlee@caltech.edu>
2016-04-16 11:15:42 -0700

    host address added for heroku

--- app/app.py
+++ app/app.py
@@ -174,17 +174,5 @@ def createIntersection(sp):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
-    app.run(port=PORT_NUMBER, debug=True)
+    app.run(host='0.0.0.0', port=PORT_NUMBER, debug=True)
