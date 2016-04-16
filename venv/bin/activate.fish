c8eedf9119a321f98d95c7e68e69c6cb65f2ae81
hatooku <jhlee@caltech.edu>
2016-04-15 20:22:30 -0700

    Reset Venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/joonheelee/Desktop/GitHub/Music/venv"
+set -gx VIRTUAL_ENV "/Users/joonheelee/Desktop/GitHub/Compatify/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
