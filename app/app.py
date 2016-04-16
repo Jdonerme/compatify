f21076d35fde6fbbce29897f4134f7151db1b663
hatooku <jhlee@caltech.edu>
2016-04-15 21:39:00 -0700

    template testing

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
