432518dc761c0588a4cd86aef573d2216145064e
Jdonerme <jdonerme@caltech.edu>
2016-04-16 01:52:20 -0700

    Improved code
    Fixed a bug, better comments, more generality, spirit of friendship

--- algs/algs.py
+++ algs/algs.py
@@ -1,72 +1,63 @@
+''' Logic for compatify. 5c spring hackathon. 4 /16/2016. coded by Joe Donermeyer
+on the team of Joonhee Lee, Catherine Ma, and Celia Zhang '''
 
-def Swag(a, b):
-    c = []
-  
-    return c
-# Given a list of dictionaries representing songs, returns a list of their song IDs. 
-def getSongsIDs(a):
+
+''' Given a list of dictionaries representing songs, returns a list of the key values
+ of the songs. '''
+def getInformation(a, key='uri'):
     c = []
     for i in range(len(a)):
-        c.append(a[i]['id'])
+        c.append(a[i][key])
     return c
-# Given a list of dictionaries representing songs, returns a list of their song names
-def getSongsNames(a):
-    c = []
-    for i in range(len(a)):
-        c.append(a[i]['name'])
-    return c
-# Given a list of dictionaries representings songs, returns a list of their artists. 
-def getArtists(a):
-    c = []
-    for i in range(len(a)):
-        c.append(a[i]['artist'])
-    return c
-# a and b are lists of dictionaries. c is a dictionary containing information of
-#The songs in common. Ignores duplicates        
-def SongsinCommon(a, b):
+''' a and b are lists of dictionaries. c is a dictionary containing information of
+The songs in common. Ignores duplicates     '''   
+def Intersection(a, b, key='uri'):
     c = []
     my_dict = {'': False}
     counted_already = {}
     for i in range(len(a)):
-        my_dict[a[i]['id']] = True  
+        my_dict[a[i][key]] = True  
     for j in range(len(b)):
-        if (my_dict.get(b[j]['id'])):
-            if (not counted_already.get(b[j]['id'])):
-                counted_already[b[j]['id']] = True
+        if (my_dict.get(b[j][key])):
+            if (not counted_already.get(b[j][key])):
+                counted_already[b[j][key]] = True
                 c.append(b[j])
     #print "There were %s songs in common. They were: \n" %len(c), c
     return c
 
-# a and b are lists. c is a list of the elements in either. Returns a list of
-#all songs in either list.
-def SongsinEither(a, b):
+''' a and b are lists. c is a list of the elements in either. Returns a list of
+all songs in either list. '''
+def Union(a, b, key = 'uri'):
     c = []
     my_dict = {'': False}
     for i in range(len(a)):
-        c.append(a[i]['id'])
-        my_dict[a[i]['id']] = True
+        c.append(a[i])
+        my_dict[a[i][key]] = True
     for j in range(len(b)):
-        if (not my_dict.get(b[j]['id'])): #make sure duplicate songs are not                             
+        if (not my_dict.get(b[j][key])): #make sure duplicate songs are not                             
             c.append(b[j])                #included twice
-    #print c
+   #print c
     return c
-# a and b are lists of dictionaries that contain song information. returns c, 
-# a list of dictionaries representing information of songs in a that aren't in  b.
-def SongsOnlyinfirst(a, b):
+''' a and b are lists of dictionaries that contain song information. returns c, 
+ a list of dictionaries representing information of songs in a that aren't in  b. '''
+def Onlyinfirst(a, b, key = 'uri'):
     c = []
     my_dict = {'': False}
     for i in range(len(b)):
-        my_dict[b[i]['id']] = True
+        my_dict[b[i][key]] = True
     for j in range(len(b)):
-        if (not my_dict.get(a[j]['id'])):                                
+        if (not my_dict.get(a[j][key])):                                
             c.append(a[j])
     #print c
     return c
-#a and b are lists of dictionaries that contain song information. Returns c, a
-#dictionary mapping how many instances of different a paramater were in common.
-# key is the paramater we're interested in
+''''a and b are lists of dictionaries that contain song information. Returns c, a
+dictionary mapping how many instances of different a paramater were in common.
+ key is the paramater we're interested in. only considers things that are in
+ common amongst songs in the intersection. Thus if 5 songs are in common between a
+ and b and the key is albums, it will produce a count of how often different albums
+ appear in that set of 5 songs.'''
 def inCommonCounts(a, b, key):
-    c = SongsinCommon(a, b)
+    c = Union(a, b)
     artistcount = {}
     for i in range(len(c)):
         if (not artistcount.get(c[i][key])):
@@ -76,9 +67,9 @@ def inCommonCounts(a, b, key):
     #print artistcount    
     return artistcount
 
-#Given a dictionary, a, mapping instances of an key to number of occurances, 
-#returns a sorted list of the keys corresponding to the top n occurances. 
-# returns as a tuple (key, occurances)
+'''Given a dictionary, a, mapping instances of an key to number of occurances, 
+returns a sorted list of the keys corresponding to the top n occurances. 
+ returns as a tuple (key, occurances) '''
 def TopNOccurances(a, n):
     c = []
     x = sorted(a, key=a.get, reverse=True)
@@ -89,25 +80,25 @@ def TopNOccurances(a, n):
     else:
         for i in range(n):
             c.append((x[i], a[x[i]]))
-    print c
+    #print c
     return c
             
 #Main function
 if __name__ == '__main__':
-    dict1 = {'id': 1, 'artist': 'Swift', 'album': '1985'}
-    dict2 = {'id': 2, 'artist': 'Kanye', 'album': 'TLOP'}
-    dict5 = {'id': 5, 'artist': 'Joon', 'album': 'Songs in the key of Junhui'}
-    dict6 = {'id': 6, 'artist': 'Swift', 'album': 'speak now'}
-    dict10 = {'id': 10, 'artist': 'Swift', 'album' : 'red'}
-    dict3 = {'id': 3, 'artist': 'Kanye', 'album' : 'TLOP'}
-    dict7 = {'id': 7, 'artist': 'Swift', 'album' : 'red'}    
-    SongsOnlyinfirst([dict1, dict2, dict5, dict6, dict10], [dict2, dict3, dict10, dict7])
-    #SongsinEither([dict1, dict2, dict5, dict6, dict10], [dict2, dict3, dict10, dict7])
-    #SongsinCommon([dict1, dict2, dict5, dict6, dict10], [dict2, dict3, dict10, dict7])
-    x = inCommonCounts([dict1, dict2, dict3, dict7, dict5, dict6, dict10], [dict1, dict2, dict3, dict5, dict6, dict10], 'album')
-    TopNOccurances(x, 40)
-    x = inCommonCounts([dict1, dict2, dict3, dict7, dict5, dict6, dict10], [dict1, dict2, dict3, dict5, dict6, dict10], 'artist')
-    TopNOccurances(x, 40)    
+    dict1 = {'uri': 1, 'artist': 'Swift', 'album': '1985'}
+    dict2 = {'uri': 2, 'artist': 'Kanye', 'album': 'TLOP'}
+    dict5 = {'uri': 5, 'artist': 'Joon', 'album': 'Songs in the key of Junhui'}
+    dict6 = {'uri': 6, 'artist': 'Swift', 'album': 'speak now'}
+    dict10 = {'uri': 10, 'artist': 'Swift', 'album' : 'red'}
+    dict3 = {'uri': 3, 'artist': 'Kanye', 'album' : 'TLOP'}
+    dict7 = {'uri': 7, 'artist': 'Swift', 'album' : 'red'}    
+    #Onlyinfirst([dict1, dict2, dict5, dict6, dict10], [dict2, dict3, dict10, dict7])
+    #print Union([dict1, dict2, dict5, dict6, dict10], [dict2, dict3, dict10, dict7])
+    #print Intersection([dict1, dict2, dict5, dict6, dict10], [dict2, dict3, dict10, dict7])
+    #x = inCommonCounts([dict1, dict2, dict3, dict7, dict5, dict6, dict10], [dict1, dict2, dict3, dict5, dict6, dict10], 'album')
+    #print TopNOccurances(x, 40)
+    #x = inCommonCounts([dict1, dict2, dict3, dict7, dict5, dict6, dict10], [dict1, dict2, dict3, dict5, dict6, dict10], 'artist')
+    #print TopNOccurances(x, 40)    
     
-    
-    
+    #y = Union([dict1, dict2, dict5, dict6, dict10], [dict2, dict3, dict10, dict7])
+    #print getInformation(y)
