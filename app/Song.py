c2247d9e0b13dbeabf6147f31ec5216e17627d51
jdonerme <jdonerme@caltech.edu>
2018-09-23 01:44:17 -0700

    remastered / stereo / mono songs no longer considered different enough to have multiple versions in shared playlist

--- app/Song.py
+++ app/Song.py
@@ -1,8 +1,17 @@
 import string, re
 sigTimeDifference = 10000 # ms
-VERSION_KEY_WORDS = ["remaster", "mono", "stereo", "version", "live", "studio",
-                     "rerecorded", "edit"]
 
+# words that are found in songs titles to differentiate versions of the same song
+DIFFERENT_VERSION_KEY_WORDS = ["version", "live", "studio", "rerecorded", "edit"]
+
+''' words that denote different versions of a song that are similar enough that
+    they should not be included twice in the interesection playlist.
+
+    They still should be removed from the title of the identifier so that all
+    versions of the song match.
+
+    '''
+SAME_VERSION_KEY_WORDS = ["remaster", "mono", "stereo", "rerecorded"]
 
 def lazy_property(fn):
     '''Decorator that makes a property lazy-evaluated.
@@ -17,17 +26,54 @@ def lazy_property(fn):
         return getattr(self, attr_name)
     return _lazy_property
 
+""" return a lower case string with punctuation removed.
+
+    """
 def simple_string(s):
     return s.lower().translate({string.punctuation: None})
 
+""" remove keywords with excess information form the song name.
+
+    """
+def get_simplified_song_name(name, keywords):
+    song_name = name
+    for word in keywords:
+            if word in song_name and ' - ' in song_name:
+                index = song_name.index(' - ')
+                song_name = song_name[:index]
+    return song_name
+
+def take_artists_from_song_name(name, artist_set):
+    song_name = name
+    feat_index = song_name.index(" (feat. ")
+
+    start_artist_index = feat_index + 8
+    end_artist_index = song_name.rfind(')')
+
+    featured_artists = song_name[start_artist_index:end_artist_index]
+
+    featured_artists_list = re.split(" & | and |, ", featured_artists)
+    artist_set |= set(featured_artists_list)
+
+    # remove the features from the title since they're not always there
+    song_name = song_name[:feat_index]
+    return song_name, artist_set
 
 
 """ A object to hold necessary song fields.
 
     uri: unique string spotify assigns to song  
     name: title of song
-    featured_artists: any collaboratin artists on the song
+    artist: primary artist on the song
+    featured_artists: any collaboration artists on the song
     duration: length of song in ms
+
+    match_name: title of the song with insignificant distinguishing features
+                removed (ex. remastered)
+    identifier: a string that should be the same for all potential different
+                versions of the same song
+
+    artist_set: A set containing all the artists that are credited on the song
 
     """
 class Song(object):
@@ -39,54 +85,43 @@ class Song(object):
         self.featured_artists = features
         self.album = album
         self.duration = duration
+
         self.artist_set = set(list(map(simple_string, [artist] + features)))
-
-        self.set_identifier()
-
-        ''' A set containing all the artists that are credited on the song.
-
-        '''
-        
+        self.set_identifier_and_match_name()
 
         # save some attributes in a dictionary for access with [] notation
         self.attributes = { "name": self.name, "uri": self.uri, "artist": 
               self.artist, "album": self.album, "duration": self.duration,
               "featured_artists": self.featured_artists,
               "identifier": self.identifier
-            }
+        }
+        
 
-    ''' A string that is unique to each song but will be the same for
-        different versions of the same song.
+    ''' Set the strings that are used for song matching.
 
         '''
-    def set_identifier(self):
+    def set_identifier_and_match_name(self):
         song_name = self.name.lower()
 
         if " (feat. " in song_name:
-            feat_index = song_name.index(" (feat. ")
-
+            song_name, artist_set = take_artists_from_song_name(song_name,
+                                                              self.artist_set)
             # if the features were not already recorded as artists for the track
             if len(self.featured_artists) == 0:
+                self.artist_set |= set(artist_set)
 
-                start_artist_index = feat_index + 8
-                end_artist_index = song_name.rfind(')')
+        # remove details about the song that are considered to not significantly
+        # make the song a different version
+        song_name = get_simplified_song_name(song_name, SAME_VERSION_KEY_WORDS)
+        self.match_name = simple_string(song_name)
 
-                featured_artists = song_name[start_artist_index:end_artist_index]
-
-                featured_artists_list = re.split(" & | and |, ", featured_artists)
-                self.artist_set |= set(featured_artists_list)
-
-            # remove the features from the title since they're not always there
-            song_name = song_name[:feat_index]
-
-        # remove version specific details from the song title 
-        for word in VERSION_KEY_WORDS:
-            if word in song_name and ' - ' in song_name:
-                index = song_name.index(' - ')
-                song_name = song_name[:index]
-        identifier = song_name.translate({string.punctuation: None}) + " - " + \
-                     self.artist.lower().translate({string.punctuation: None})
+        # remove any version specific details from the song title 
+        song_name = get_simplified_song_name(song_name, DIFFERENT_VERSION_KEY_WORDS)
+        
+        identifier = simple_string(song_name) + " - " + \
+                     simple_string(self.artist)
         self.identifier = identifier
+
 
     @lazy_property
     def audio_features(self):
@@ -94,20 +129,11 @@ class Song(object):
         return self.sp.audio_features([self.uri])
 
 
-    def has_similar_features_with(self, other):
-        if self.identifier == other.identifier:
-            return True
-
-        return False
-
     ''' 
-        How to consider two songs as the same. If the uri is the same the track
+        How to consider two songs as the same. If the uri is the same, the track
         is the same for sure.
 
-        If there are collaborating artists in one version but not the other,
-        it is considered a different version of the song.
-
-        If the name and artist are the same and the duration is within a number
+        If the name and artists are the same and the duration is within a number
         of seconds, the song is considered the same too.
 
         '''
@@ -116,9 +142,10 @@ class Song(object):
             if self.uri == other.uri:
                 return True 
 
-            elif simple_string(self.name) == simple_string(other.name) and \
+            elif self.match_name == other.match_name and \
                 self.artist_set == other.artist_set and \
                 (abs(self.duration - other.duration) < sigTimeDifference):
+
                 return True
 
             return False
